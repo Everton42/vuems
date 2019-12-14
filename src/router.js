003fe17408d71aac232bbf868da84dea77fc8d38
Everton Ferreira <everton.ferreira@hotmail.com>
2019-12-14 10:05:35 -0300

    default path

--- src/router.js
+++ src/router.js
@@ -9,6 +9,10 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '*',
+      redirect: '/home',
+    },
+    {
       path: '/home',
       name: 'home',
       component: Home,
@@ -19,6 +23,6 @@ export default new Router({
       name: 'moviesList',
       props: true,
       component: () => import('./components/MoviesList.vue')
-    },
+    }
   ]
 })
